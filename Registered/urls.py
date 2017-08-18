83af449a4e9f2b90c3aec52f5c0b4ba114d0ed58
vpadillar <vpadillar8@gmail.com>
2017-08-18 18:29:04 -0500

    arreglo css merge

--- Registered/urls.py
+++ Registered/urls.py
@@ -17,16 +17,10 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
-<<<<<<< HEAD
 from django.conf import settings
 from django.conf.urls.static import static
-
-
-=======
 import settings
 from django.conf.urls.static import static
-
->>>>>>> 0c9c9b70a117485bd7a12d92e0620a5a1d343ccc
 
 
 urlpatterns = [
