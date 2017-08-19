6e14bd4202d06a12054a481647ba396e12f8753c
darkdrei <darkdrei88@gmail.com>
2017-08-19 05:38:45 -0500

    adicionar el listado de domiciliarios que rstan por ser asignados

--- operacion/static/operacion/js/modallabor.js
+++ operacion/static/operacion/js/modallabor.js
@@ -1,7 +1,21 @@
 $(document).on('ready', function(){
+  $('#mensaje_empleado').modal();
   $('.addlabormodal').on('click', function(event){
     document.getElementById('form_labor').reset();
-    $('#addlabor').modal('open');
+    $.ajax({
+      url:'/motorizado/list/motorizado/',
+      type:'get',
+      dataType:'json',
+      success:function(data){
+         var res = data['object_list'];
+          console.log(data,"   ",res);
+          if (res.length == 0){
+            $('#mensaje_empleado').modal('open');
+          }else{
+            $('#addlabor').modal('open');
+          }
+      }
+    });
   })
 
 });
