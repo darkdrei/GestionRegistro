c16ced4a44821ae1c9db2682f16b181a395b38bc
darkdrei <darkdrei88@gmail.com>
2017-08-19 07:27:57 -0500

    modificacion de la funcionalidad de validacion de envio de labor

--- operacion/static/operacion/js/modallabor.js
+++ operacion/static/operacion/js/modallabor.js
@@ -8,10 +8,17 @@ $(document).on('ready', function(){
       dataType:'json',
       success:function(data){
          var res = data['object_list'];
-          console.log(data,"   ",res);
+          console.log(data,"   ",res,"  ",res.length);
           if (res.length == 0){
             $('#mensaje_empleado').modal('open');
           }else{
+            $('#empleado').material_select('destroy');
+            var men="<option value=\"0\" disabled selected>Seleccione Trabajador</option>";
+            for(var i=0;i < res.length; i++ ){
+              men+="<option value=\""+res[i].emp_id+"\">"+res[i].nombre+" "+res[i].apellidos+"</option>";
+            }
+            $('#empleado').html(men);
+            $('#empleado').material_select();
             $('#addlabor').modal('open');
           }
       }
