4c301605926d667ea02992013e4827c039b13018
darkdrei <darkdrei88@gmail.com>
2017-11-16 19:07:30 -0500

    actualizacion

--- Registered/settings.py
+++ Registered/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'Registered.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'register2',
+        'NAME': 'register',
         'USER': 'postgres',
         'PASSWORD': '123456',
         'HOST': '127.0.0.1',
@@ -109,7 +109,7 @@ DATABASES = {
         'POST': '5432'
     },
 }
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
